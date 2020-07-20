7f1d929288992280d92653dc326757e21ed7acd7
suchi2001 <68218245+suchi2001@users.noreply.github.com>
2020-07-20 13:11:07 +0530

    Create DAY-1.py

---
+++ newfolder/Python/DAY-1.py
@@ -0,0 +1,227 @@
+Question:1
+What is python??
+Ans:
+Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+
+It is used for:
+
+web development (server-side),
+software development,
+mathematics,
+system scripting.
+
+What can Python do?
+Python can be used on a server to create web applications.
+Python can be used alongside software to create workflows.
+Python can connect to database systems. It can also read and modify files.
+Python can be used to handle big data and perform complex mathematics.
+Python can be used for rapid prototyping, or for production-ready software development.
+
+Why Python?
+Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc).
+Python has a simple syntax similar to the English language.
+Python has syntax that allows developers to write programs with fewer lines than some other programming languages.
+Python runs on an interpreter system, meaning that code can be executed as soon as it is written. This means that prototyping can be very quick.
+Python can be treated in a procedural way, an object-orientated way or a functional way.
+Good to know
+The most recent major version of Python is Python 3, which we shall be using in this tutorial. 
+However, Python 2, although not being updated with anything 
+other than security updates, is still quite popular.
+In this tutorial Python will be written in a text editor. 
+It is possible to write Python in an Integrated Development Environment, such as Thonny, Pycharm, Netbeans or Eclipse which are particularly 
+useful when managing larger collections of Python files.
+Python Syntax compared to other programming languages
+Python was designed for readability, and has some similarities to the English language with influence from mathematics.
+Python uses new lines to complete a command, as opposed to other programming languages which often use semicolons or parentheses.
+Python relies on indentation, using whitespace, to define scope; such as the scope of loops, functions and classes.
+Other programming languages often use curly-brackets for this purpose.
+How to install the python in your pc:
+1.Click Python Download.
+2.Click the Windows link (two lines below the Download Python 3.7.4 button). The following page will appear in your browser
+3.Click on the Download Windows x86-64 executable installer link under the top-left Stable Releases.
+The following pop-up window titled Opening python-3.74-amd64.exe will appear.
+Click the Save File button.
+The file named python-3.7.4-amd64.exe should start downloading into your standard download folder. 
+This file is about 30 Mb so it might take a while to download fully if you are on a slow internet connection (it took me about 10 seconds over a cable modem).
+The file should appear as
+4.Move this file to a more permanent location, so that you can install Python (and reinstall it easily later, if necessary).
+5.Feel free to explore this webpage further; if you want to just continue the installation, you can terminate the tab browsing this webpage.
+6.Installing
+Double-click the icon labeling the file python-3.7.4-amd64.exe.
+A Python 3.7.4 (64-bit) Setup pop-up window will appear.
+
+
+
+Ensure that the Install launcher for all users (recommended) and the Add Python 3.7 to PATH checkboxes at the bottom are checked.
+
+If the Python Installer finds an earlier version of Python installed on your computer, the Install Now message may instead appear as Upgrade Now (and the checkboxes will not appear).
+
+Highlight the Install Now (or Upgrade Now) message, and then click it.
+When run, a User Account Control pop-up window may appear on your screen. I could not capture its image, but it asks, Do you want to allow this app to make changes to your device.
+
+Click the Yes button.
+A new Python 3.7.4 (64-bit) Setup pop-up window will appear with a Setup Progress message and a progress bar.
+
+
+
+During installation, it will show the various components it is installing and move the progress bar towards completion. Soon, a new Python 3.7.4 (64-bit) Setup pop-up window will appear with a Setup was successfuly message.
+
+
+
+Click the Close button.
+Python should now be installed.
+
+Verifying
+To try to verify installation,
+Navigate to the directory C:\Users\Pattis\AppData\Local\Programs\Python\Python37 (or to whatever directory Python was installed: see the pop-up window for Installing step 3).
+Double-click the icon/file python.exe.
+The following pop-up window will appear.
+
+
+
+A pop-up window with the title C:\Users\Pattis\AppData\Local\Programs\Python\Python37\python.exe appears, and inside the window; on the first line is the text Python 3.7.4 ... (notice that it should also say 64 bit). Inside the window, at the bottom left, is the prompt >>>: type exit() to this prompt and press enter to terminate Python.
+
+You should keep the file python-3.7.4.exe somewhere on your computer in case you need to reinstall Python (not likely necessary).
+
+You may now follow the instructions to download and install Java (you should have already installed Java, but if you haven't, it is OK to do so now, so long as you install both Python and Java before you install Eclipse), and then follows the instruction to download and install the Eclipse IDE. Note: you need to download/install Java even if you are using Eclipse only for Python)
+
+Start the Installing instructions directly below.
+
+
+Question:2
+What is code Environment? and thier types.
+Ans:
+Python… the most popular and favorite programming language in the world for programmers of all age groups. If you are a beginner this language is strongly recommended to learn first. Well, In programming, we all know the importance of code editor and IDEs to write our program and to execute it but choosing the best code editor or IDE is always a confusing question. Understand that the best code editor or IDE depends on so many things such as programming language, project type, project size, OS support and considering a lot of other features. If we talk about Python so this language is also not an exception.
+
+
+
+We are going to discuss the code editor or IDEs for Python to use in 2020. This information is going to be based on the opinion given by experienced developers, public web data, some previous surveys like Python Developers Survey 2018 Results / Editors and IDEs and the most important person Guido van Rossum (Python Creator). Firstly understand that IDE and code editor both are different things.
+
+Text/Code Editor: Code editors are the lightweight tool that allows you to write and edit the code with some features such as syntax highlighting and code formatting. It provided fewer features than IDE.
+Integrated Development Environment (IDE): IDEs are full-fledged environment which provide all the essential tools needed for software development. It just doesn’t handle the code (for example, write, edit, syntax highlighting and auto-completion) but also provides other features such as debugging, execution, testing, and code formatting that helps programmers.
+Let’s start with some overview of the best code editor based on certain terms…
+
+What is Your Level?
+
+
+
+
+Beginner — IDLE (or Online Python Editors) is perfect choice for the first steps in python language. PyCharm is also good but takes the help of some experienced person while using this.
+Intermediate — PyCharm, Sublime, Atom, Vs Code.
+Advanced — PyCharm, Vim, Emacs, Sublime, Atom, Vs Code.
+What’s Your End Goal?
+
+Web development — PyCharm Professional, VS Code
+Data Science — Spyder, Jupyter Notebook, PyCharm Professional
+Scripting — Sublime, Atom, PyCharm Community, Eclipse + PyDev
+QA — Sublime, Atom, PyCharm Community, Jupyter Notebook
+What is Your Environment/OS?
+
+Linux, macOS — PyCharm, Sublime, Atom, Vim, Jupyter
+Windows — Sublime, VS Code, Eclipse + PyDev, PyCharm
+Multiple/mixed OS — PyCharm, Sublime, Atom
+Which Hardware Do You Have?
+
+Bad — IDLE, Atom, Sublime, Online Editor
+Good — PyCharm, VS Code, Eclipse + PyDev
+Note: We have considered limited terms but it also depends on budget, git integration, teamwork, previous programming knowledge.
+
+List of Best Python IDE
+1. PyCharm
+In industries most of the professional developers use PyCharm and it has been considered the best IDE for python developers. It was developed by the Czech company JetBrains and it’s a cross-platform IDE. It gives daily tips to improve your knowledge of how you can use it more efficiently which is a very good feature. It comes in two versions community version and a professional version where community version is free but the professional version is paid. Below are some other features of this IDE.
+
+It is considered as an intelligent code editor, fast and safe refactoring, and smart code.
+Features for debugging, profiling, remote development, testing the code, auto code completion, quick fixing, error detection and tools of the database.
+Support for Popular web technologies, web frameworks, scientific libraries and version control.
+2. Spyder
+Spyder is another good open-source and cross-platform IDE written in Python. It is also called Scientific Python Development IDE and it is the most lightweight IDE for Python. It is mainly used by data scientists who can integrate with Matplotlib, SciPy, NumPy, Pandas, Cython, IPython, SymPy, and other open-source software. It comes with the Anaconda package manager distribution and it has some good advanced features such as edit, debug, and data exploration. Below are some other features of this IDE.
+
+Auto code completion and syntax highlighting.
+Ability to search and edit the variables from the graphical user interface itself.
+Static code analysis
+It is very efficient in tracing each step of the script execution by a powerful debugger.
+3. Eclipse PyDev
+Eclipse is one of the most popular IDE among developers which is written in Java but you can install Pydev plugin in eclipse and use it for Python as well. The primary focus of this IDE is the analysis of code, debugging in the graphical pattern, refactoring of python code, etc. Eclipse PyDev is stable and provides good performance for most of the python project life cycle. Below are some other features of this IDE.
+
+Pydev supports Django integration, Unittest integration, PyLint integration
+Code folding and code completion with auto import
+Good syntax high lighting and remote debugger
+Interactive console
+Allows you to create a Google App Engine (GAE) Python project
+4. IDLE
+IDLE is a cross-platform open-source IDE that comes by default with Python so you don’t need to worry about the installation or setup. IDLE is written in Python and this IDE is suitable for beginner level developers who want to practice on python development. IDLE is lightweight and simple to use so you can build simple projects such as web browser game automation, basic web scraping applications, and office automation. This IDE is not good for larger projects so move to some advance IDEs after learning the basics from IDLE.
+
+
+
+
+Python shell with syntax highlighting
+Call stack’s clear visibility
+Multi-window code editor that allows features like smart indentation, autocomplete, etc
+It has an interactive interpreter with colorizing of input, output, and error messages.
+Program animation or stepping.
+5. Wing
+Wing IDE is created by Wingware and it is faster, stable and extremely lightweight cross-platform Python IDE. It comes in three editions:
+
+Wing Pro (Free Trial): A full-featured commercial version, for professional programmers.
+Wing Personal (Paid): Free version that omits some features, for students and hobbyists.
+Wing 101 (Paid): A very simplified free version, for beginners in programming.
+This IDE comes with a strong debugger and smart editor that makes the interactive Python development speed, accurate and fun to perform. Some of its main features are given below…
+
+Automatic multi-process, child process, remote debug process and refactoring, etc.
+Test-driven development with various frameworks like the unittest, pytest, nose, doctest, and Django testing.
+It also has auto code completion in which the error is displayed in a feasible manner and line editing is also possible.
+Remote development support.
+List of Best Python Code Editor
+1. Emacs
+Emacs was created in 1976 by Richard Stallman. It is free and fully customizable software available on all platforms. Emacs uses a form of the powerful Lisp programming language for customization, and various customization scripts exist for Python development.
+
+Syntax highlighting to differentiate document elements such as keywords and comments.
+Automatic indentation such as spaces, newlines, and brackets for consistent formatting in a file.
+24-bit color encoded support for the terminals supporting it.
+Guido van Rossum (Python creator) accepted Emacs his favourite text editor in one of his tweet in 2016 Emacs of course!. We also took his recent opinion about his favorite text editor and Emacs is still his favorite one.
+
+Reply-to-Anu-Upadhyay-By-Guido-van-Rossum
+In the book “The Art of UNIX Programming”
+
+Emacs is undoubtedly the most powerful programmer’s editor in existence. It’s a big, feature-laden program with a great deal of flexibility and customizability.
+
+2. Visual Studio Code
+Visual Studio Code (VS Code) is a free and open-source code editor created by Microsoft that can be used for Python development. You can add the extension to create a Python development environment. It provides support for debugging, embedded Git control, syntax highlighting, IntelliSense code completion, snippets, and code refactoring. Some of it’s best features are given below.
+
+Thousands of plugins/extensions available through the VS Code Marketplace.
+Powerful debugger by which the user can debug code from the editor itself.
+Easily customizable.
+Multi-platform, multi-language support, multi-split window feature and vertical orientation.
+3. Sublime Text:
+Written by a Google engineer sublime text is a cross-platform IDE developed in C++ and Python. It has basic built-in support for Python. Sublime text is fast and you can customize this editor as per your need to create a full-fledged Python development environment. You can install packages such as debugging, auto-completion, code linting, etc. There are also various packages for scientific development, Django, Flask and so on. Some of its best features are given below…
+
+Goto anything for opening files with few clicks and can navigate to words or symbols.
+Python-based plugin API.
+Syntax highlighting and allows simultaneous editing (multiple selections)
+Command Palette implementation that accepts text input from users.
+High performance, block selection and simultaneous editing (multiple selections).
+4. Atom
+Atom is an open-source cross-platform IDE built using web technologies. It is based on a framework built by GitHub named Electron. Atom is highly customizable and provides Python language support installing the extension when Atom is running. Some of the good packages for Python development are atom-python-run, Python Black, python-indent, atom-python-test, autocomplete-python, Python Tools, linter-flake8, python-debugger, etc. Below are some nice features of the Atom.
+
+Enables support for third-party packages
+Lightweight, smart auto-completion, multi-language support with good syntax highlighting
+Multiple panes and themes
+Allows installation and management of packages
+5. Vim
+Vim is an open-source, cross-platform text editor. It is pre-installed in macOS and UNIX systems but for Windows, you need to download it. This text editor can be used as a command-line interface as well as a standalone application. Vim is extremely popular in geeks communities and by adding extensions or modifying its configuration file you can easily adapt it for development in Python. There are big lists of plugins, features, and versions of Vim. Some of its good features are listed below…
+
+Very stable and lightweight.
+Plugins are available for syntax highlighting, code completion, debugging, refactoring, etc
+It has a powerful integration, search and replace functionality.
+It is very persistent and also have a multilevel undo tree.
+Honorable Mentions
+We have mentioned all the IDEs and text editor for Python development but there is one popular web application or tool which is mainly used for data science projects and i.e. Jupyter Notebook. Let’s see the introduction and some of its features…
+
+Jupyter Notebook: Jupyter Notebook is a web-based interactive development environment; It’s well known in the data science community for analyzing, sharing and presenting the information. It is easy to use, open-source software that allows you to create and share live code, visualizations, etc. Some of its good features are given below…
+
+Support for Numerical simulation, data cleaning machine learning data visualization, and statistical modeling.
+Markdown and HTML integration.
+Integrated data science libraries (matplotlib, NumPy, Pandas).
+It offers you to see and edit your code to create powerful presentations.
+You can also convert your complete work into PDF and HTML files, or you can just export it as a .py file.
+Starting and stoping servers, opening folders and files.
